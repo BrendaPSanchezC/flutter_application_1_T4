eb0faaa0e89c50b4218dd58ff138476e22c94f92
BrendaPSanchezC <le20080934@merida.tecnm.mx>
2024-04-21 22:30:01 -0600

    Terminado

--- lib/models/nav_bar.dart
+++ lib/models/nav_bar.dart
@@ -7,8 +7,8 @@ class NavBar {
 }
 
 const itemsNavBar = const [
-  NavBar(path: 'assets/icons/icons8-home.svg', name: 'Home'),
-  NavBar(path: 'assets/icons/icons8-search.svg', name: 'Search'),
+  NavBar(path: 'assets/icons/home.svg', name: 'Home'),
+  NavBar(path: 'assets/icons/search.svg', name: 'Search'),
   NavBar(path: 'assets/icons/star-outline.svg', name: 'Favorites'),
   NavBar(path: 'assets/icons/auser.svg', name: 'Profile'),
 ];
